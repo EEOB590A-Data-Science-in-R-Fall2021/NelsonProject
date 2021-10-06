11616ff7ed677ded826a5b716172fe2aa348c13c
Jessica <jnel@iastate.edu>
2021-10-06 12:00:35 -0500

    part 3

---
+++ 590A_assignments/Data_wrangling_part3_PracticeExercise.R
@@ -0,0 +1,34 @@
+# EEOB590A - Data_wrangling part 3 practice exercise ------
+
+# Part 1: Get set up ----------
+
+## 1.1) Load libraries ----------
+
+## 1.2) Read in data ----------
+# From the tidy folder, read in the file you created from last week's assignment on the pollination dataset ("poll_long_tidy.csv")
+
+## 1.3) Change the class of each variable if needed ------
+
+# Part 2: Subset & summarize --------
+
+## 2.1) Make a new dataframe with just the data from Guam at the racetrack site and name accordingly. --------
+
+## 2.2) Make a new dataframe with just the uniqueID, island, site, transect, insectorder, numinsects, and duration columns. --------
+
+## 2.3) With the full database (not the new ones you created in the two previous steps), summarize data, to get: --------
+
+### 2.3.a) a table with the total number of insects at each site, and then arrange rows in descending order --------
+
+### 2.3.b) a table that shows the mean number of insects per island, arranged in ascending (smallest first) order --------
+
+### 2.3.c) a table that shows the min and max number of insects per transect (note that the transects have the same name at each site) --------
+
+## 2.4) Figure out which insect order is found across the greatest number of sites and has the most total insects --------
+
+
+## 2.5) For the insect order with the greatest total number of insects and found at the most sites, calculate the mean and sd by site. Include the island name in the final table. --------
+
+
+## 2.6) Ask a question about the relationship between bowl color and insectorder, and then write the code to answer your question. ------
+
+
