19ab235f3e1bcb1fa04c653aff99da548d2461fb
Rogers <haldre@ee-hrog-mba20.local>
2021-10-20 07:29:39 -0500

    moved files into 590A assignments

--- 590A_assignments/CodingWithStyle_exercise.R
+++ 590A_assignments/CodingWithStyle_exercise.R
@@ -1,30 +1,26 @@
-# Contributor: Jessica Nelson
-# Date: 9/14/2021
-
 #Coding with Style 
 #Fix this script, using good coding convention. 
 #This is a made-up dataset, so you can create any story and associated meaningful variable and filenames to go with it. 
 #Put this script into the format for the Outline feature in RStudio
-# 1. Create dataset ----
-birds <-data.frame(state=c('Iowa','Vermont','Hawaii','Texas','Alaska'),
-                bird=c('american goldfinch','hermit thrush','nene','northern mockingbird','willow ptarmigan'),
+#create dataset
+df1<-data.frame(state=c('Iowa','Vermont','Hawaii','Texas','Alaska'),
+                var1=c('american goldfinch','hermit thrush','nene','northern mockingbird','willow ptarmigan'),
                 snow=c(1,1,0,0,1),
                 catCount=sample(3:10, 5, replace=T))
 
-# 2. Explore dataset ----
+#explore dataset
 library(ggplot2)
 
-ggplot(birds,aes(state,catCount))+
-  geom_point()
+ggplot(df1,aes(state,catCount))+geom_point()
 
-# 3. Create new column for maximum ----
-birds$max = 10
+#create new column
+df1$max = 10
 
-# 4. Analyze dataset ----
+#analyze dataset
 library(lme4)
 
-bird_mod<-glm(snow ~ catCount, family=binomial, data=birds)
-summary(bird_mod)
+mod1<-glm(snow ~ catCount, family=binomial, data=df1)
+summary(mod1)
 
-# 5. Create a csv file for the dataset
-write.csv(birds, "data/tidy/bird_count.csv")
+#create a csv file
+write.csv(df1, "data/tidy/database1.csv")
